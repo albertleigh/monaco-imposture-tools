9291743e5a21d7a420ee2eacbb9b7f4bf341653a
Albert Leigh <liwentao90@yahoo.com>
2026-03-17 14:33:47 +0800

    Release/0.6.0 (#43)
    * support return returnType descriptions and display
    * release 0.6.0

---
+++ CHEATSHEET.md
@@ -0,0 +1,2 @@
+lerna publish 0.6.0 --no-changelog --no-git-tag-version --no-push
+lerna version 0.6.0 --no-changelog --no-git-tag-version --no-push
--- languages/monaco-az-logic-app-lang/src/index.ts
+++ languages/monaco-az-logic-app-lang/src/index.ts
@@ -6,6 +6,7 @@ export {
   ErrorHandler,
 } from './base';
 export {
+  DescriptionType,
   ValueDescription,
   FunctionValueDescription,
   OverloadedFunctionValueDescription,
